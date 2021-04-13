7021f59b72ad66d220ce161b673c38fc2497e468
Leonardo Grandi <leonardofelipegrandi@gmail.com>
2021-04-12 22:16:19 -0300

    chore: add config sonar

--- jest.config.ts
+++ jest.config.ts
@@ -159,7 +159,7 @@ export default {
   // testRegex: [],
 
   // This option allows the use of a custom results processor
-  // testResultsProcessor: undefined,
+  testResultsProcessor: 'jest-sonar-reporter',
 
   // This option allows use of a custom test runner
   // testRunner: "jasmine2",
